702238dfbab58479a768e0056de4adde7f13e2a7
Shreeya Bansal <shreeyabansal77@gmail.com>
2026-07-23 00:04:22 +0530

    hash

--- starPattern.py
+++ starPattern.py
@@ -19,3 +19,8 @@ if __name__ == "__main__":
     N=5
     sol.pattern(N)
 
+#For upside down pyramid
+#Run an outer loop (i) from 0 to N-1 for rows.
+#Print i spaces before the stars.
+#Print 2 * N - (2 * i + 1) stars.
+#Print i spaces after the stars (optional for symmetry in visualization).
