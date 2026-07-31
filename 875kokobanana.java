2e6e403317f03267597e3a4dbe12e8af12481afd
Shreeya Bansal <shreeyabansal77@gmail.com>
2026-07-31 23:15:59 +0530

    youknowit

--- 875kokobanana.java
+++ 875kokobanana.java
@@ -17,6 +17,7 @@ class Solution {
     }
 }
 //added stuff
+//i hate myself
 
 /*Koko loves to eat bananas. There are n piles of bananas, the ith pile has piles[i] bananas. The guards have gone and will come back in h hours.
 
