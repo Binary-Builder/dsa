a56a5c8412a6996448f084aaeca8a24ca474ce4b
Shreeya Bansal <shreeyabansal77@gmail.com>
2026-08-02 23:50:59 +0530

    corrections

--- palindrome.java
+++ palindrome.java
@@ -7,6 +7,6 @@ class Solution {
         for ( ; y < x; x /= 10){
             y = y * 10 + x % 10; // checking two halves
         }
-        return x == y || x == y/10 //if no. is odd, remove the middle number and then compare
+        return x == y || x == y/10; //if no. is odd, remove the middle number and then compare
     }
 }
