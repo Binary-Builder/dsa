1a967797c6e065e93f96ad9c95d646c9e71340ea
Shreeya Bansal <shreeyabansal77@gmail.com>
2026-08-02 23:50:11 +0530

    palindrome

---
+++ palindrome.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isPalindrome (int x){
+        if ( x < 0 || (x > 0 && x % 10 == 0)) { // if negative OR ends with 0
+            return false;
+        }
+        int y = 0;
+        for ( ; y < x; x /= 10){
+            y = y * 10 + x % 10; // checking two halves
+        }
+        return x == y || x == y/10 //if no. is odd, remove the middle number and then compare
+    }
+}
